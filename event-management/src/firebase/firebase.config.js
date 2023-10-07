03989b4a3ba35364dbd8a42bd0d6827403e2bb4d
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-10-07 23:28:14 +0600

    added env.local file

--- event-management/src/firebase/firebase.config.js
+++ event-management/src/firebase/firebase.config.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4W3Ny3-UdiMECCXm08nIfsQfYnCUSkTU",
-  authDomain: "sinisterspades-auth.firebaseapp.com",
-  projectId: "sinisterspades-auth",
-  storageBucket: "sinisterspades-auth.appspot.com",
-  messagingSenderId: "356050768866",
-  appId: "1:356050768866:web:089f3dc1c10cb263e05fbb"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
