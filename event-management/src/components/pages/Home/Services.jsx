87592d197f313aa9d92e5551cce0a91322d6b685
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-10-07 10:09:18 +0600

    added card image describtion price and button

--- event-management/src/components/pages/Home/Services.jsx
+++ event-management/src/components/pages/Home/Services.jsx
@@ -22,8 +22,29 @@ const Services = () => {
               Organize and host competitive gaming tournaments with professional
               setup and management
             </p>
-            <p>price: $ 500</p>
-            <div className="card-actions justify-center mt-5">
+            <div className="card-actions justify-center items-center mt-5">
+              <p>price: $ 500</p>
+              <button className="btn btn-primary">More Details</button>
+            </div>
+          </div>
+        </div>
+        {/* card 2 */}
+        <div className="card w-96 glass">
+          <figure>
+            <img
+              src="https://i.ibb.co/g79K8cw/ella-don-f-K5-Oomnc-Wk-unsplash.jpg"
+              alt="car!"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">LAN Party Setup</h2>
+            <p>
+              Set up a local area network for a large-scale gaming event,
+              including equipment and technical support.
+            </p>
+
+            <div className="card-actions justify-center items-center mt-5">
+              <p>price: $ 800</p>
               <button className="btn btn-primary">More Details</button>
             </div>
           </div>
@@ -31,18 +52,18 @@ const Services = () => {
         <div className="card w-96 glass">
           <figure>
             <img
-              src="https://i.ibb.co/n8W8NMF/stem-list-EVgs-Ab-L51-Rk-unsplash.jpg"
+              src="https://i.ibb.co/wNFGbpZ/jesus-loves-austin-0-O0gux4-OZX4-unsplash.jpg"
               alt="car!"
             />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Tournament Hosting</h2>
+            <h2 className="card-title">Live Streaming Service</h2>
             <p>
-              Organize and host competitive gaming tournaments with professional
-              setup and management
+              Provide high-quality live streaming services for gaming events,
+              ensuring a seamless viewing experience.
             </p>
-            <p>price: $ 500</p>
-            <div className="card-actions justify-center mt-5">
+            <div className="card-actions justify-center items-center mt-5">
+              <p>price: $ 500</p>
               <button className="btn btn-primary">More Details</button>
             </div>
           </div>
@@ -50,18 +71,18 @@ const Services = () => {
         <div className="card w-96 glass">
           <figure>
             <img
-              src="https://i.ibb.co/n8W8NMF/stem-list-EVgs-Ab-L51-Rk-unsplash.jpg"
+              src="https://i.ibb.co/vVdqfQg/F487ca-DWYAEldqw.jpg"
               alt="car!"
             />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Tournament Hosting</h2>
+            <h2 className="card-title">Prize Pool Management</h2>
             <p>
-              Organize and host competitive gaming tournaments with professional
-              setup and management
+              Handle the distribution of prizes, cash, and merchandise for
+              tournament winners and participants.
             </p>
-            <p>price: $ 500</p>
-            <div className="card-actions justify-center mt-5">
+            <div className="card-actions justify-center items-center mt-5">
+              <p>price: $ 200</p>
               <button className="btn btn-primary">More Details</button>
             </div>
           </div>
@@ -69,18 +90,19 @@ const Services = () => {
         <div className="card w-96 glass">
           <figure>
             <img
-              src="https://i.ibb.co/n8W8NMF/stem-list-EVgs-Ab-L51-Rk-unsplash.jpg"
+              src="https://i.ibb.co/4W3161R/stem-list-ry-RU-cd1yas-unsplash.jpg"
               alt="car!"
             />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Tournament Hosting</h2>
+            <h2 className="card-title">Custom Event Branding</h2>
             <p>
-              Organize and host competitive gaming tournaments with professional
-              setup and management
+              Design and implement custom branding for your gaming event,
+              including logos, banners, and marketing materials
             </p>
-            <p>price: $ 500</p>
-            <div className="card-actions justify-center mt-5">
+
+            <div className="card-actions justify-center items-center">
+              <p>price: $ 300</p>
               <button className="btn btn-primary">More Details</button>
             </div>
           </div>
@@ -88,37 +110,20 @@ const Services = () => {
         <div className="card w-96 glass">
           <figure>
             <img
-              src="https://i.ibb.co/n8W8NMF/stem-list-EVgs-Ab-L51-Rk-unsplash.jpg"
+              src="https://i.ibb.co/SXBcdCj/mimi-thian-ZKBzlifgkgw-unsplash.jpg"
               alt="car!"
             />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Tournament Hosting</h2>
+            <h2 className="card-title">
+              Tournament HostingTechnical Support Team
+            </h2>
             <p>
-              Organize and host competitive gaming tournaments with professional
-              setup and management
+              Provide on-site technical support and troubleshooting for gaming
+              setups and equipment during the event
             </p>
-            <p>price: $ 500</p>
-            <div className="card-actions justify-center mt-5">
-              <button className="btn btn-primary">More Details</button>
-            </div>
-          </div>
-        </div>
-        <div className="card w-96 glass">
-          <figure>
-            <img
-              src="https://i.ibb.co/n8W8NMF/stem-list-EVgs-Ab-L51-Rk-unsplash.jpg"
-              alt="car!"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Tournament Hosting</h2>
-            <p>
-              Organize and host competitive gaming tournaments with professional
-              setup and management
-            </p>
-            <p>price: $ 500</p>
-            <div className="card-actions justify-center mt-5">
+            <div className="card-actions justify-center items-center mt-5">
+              <p>price: $ 600</p>
               <button className="btn btn-primary">More Details</button>
             </div>
           </div>
